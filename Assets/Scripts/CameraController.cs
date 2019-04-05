c169f4ab741f595cf1da5734df63b5a17b5bb1e9
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-04-05 23:22:33 +0100

    Camera follows player/script ordering/debugging always generates new player object :D

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+    // Use this for initialization
+    void Start() {
+        target = PlayerController.instance.transform;
+    }
+
+    // LateUpdate is called once per frame after all Updates
+    void LateUpdate() {
+        this.transform.position = new Vector3(target.position.x,target.position.y,this.transform.position.z);
+    }
+}
