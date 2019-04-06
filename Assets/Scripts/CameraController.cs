d338f41b6bbf39678035faeb36d3b59be68494b7
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-04-06 13:16:04 +0100

    Switch to latest unity / smoothed player camera / tileset cr

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public Transform target;
+    public float smoothSpeed = 0.125f;
 
     // Use this for initialization
     void Start() {
         target = PlayerController.instance.transform;
     }
 
-    // LateUpdate is called once per frame after all Updates
+    // LateUpdate is called once per frame after all Updates    
     void LateUpdate() {
-        this.transform.position = new Vector3(target.position.x,target.position.y,this.transform.position.z);
+        Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, this.transform.position.z);
+        Vector3 smoothedPosition = Vector3.Lerp(this.transform.position,desiredPosition,smoothSpeed);
+
+        this.transform.position = smoothedPosition;
     }
 }
