b99aca7cb9d9236e0dd2c89afb1efe1d7b8b65a9
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-06-12 19:40:05 +0100

    Player no longer clips halfway through map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -59,7 +59,7 @@ public class PlayerController : MonoBehaviour {
         by the bounds
         -Function called by Scenes' CameraController
          */
-        this.bottomLeft = bottomLeft;
-        this.topRight = topRight;
+        this.bottomLeft = bottomLeft + new Vector3(1f,1f,0f);
+        this.topRight = topRight + new Vector3(-1f, -1f, 0f);
     }
 }
