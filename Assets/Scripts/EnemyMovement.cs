6e77a865e8052d9a9537b5d0513c28cbc3e1a930
Thomas Huynh <tigereyez57@gmail.com>
2019-06-12 10:24:46 +0100

    more tiles

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float enemySpeed;
+    Transform EPosition;
+    Rigidbody2D Ebody;
+    float EWidth;
+    public LayerMask EMask;
+
+	// Use this for initialization
+	void Start () {
+        EPosition = this.transform;
+        Ebody = this.GetComponent<Rigidbody2D>();
+        EWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector2 lineCastPos = EPosition.position - transform.right * EWidth;
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, EMask);
+
+        if (!isGrounded)
+        {
+            Vector3 Erotate = EPosition.eulerAngles;
+            Erotate.y += 180;
+            EPosition.eulerAngles = Erotate;
+        }
+
+        Vector2 EVel = Ebody.velocity;
+        EVel.x = -EPosition.right.x * enemySpeed;
+        Ebody.velocity = EVel;
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Slime : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public Animator NPCAnimator;
+    public float moveSpeed;
+
+    //public static PlayerController instance; // to prevent duplicate players when re-entering spawn zone
+    public string areaTransitionName = null;
+    void Update()
+        {
+
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, Input.GetAxisRaw("Vertical") * moveSpeed);
+
+            NPCAnimator.SetFloat("moveX", theRB.velocity.x);
+            NPCAnimator.SetFloat("moveY", theRB.velocity.y);
+
+            if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+            {
+
+                NPCAnimator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+                NPCAnimator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            }
+        }
+}
