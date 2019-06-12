d2937b165c0121a3f7e0e8e48fcb95f8b1a1cc0c
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-06-12 20:06:37 +0100

    black screen fading when changing maps

---
+++ Assets/Scripts/UIFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFade : MonoBehaviour {
+
+    public Image fadeScreen;
+    public float fadeSpeed = 1f;
+
+    private bool shouldFadeToBlack;
+    private bool shouldFadeFromBlack;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        if (shouldFadeToBlack) {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b,1f);
+        }
+        if (shouldFadeFromBlack) {
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
