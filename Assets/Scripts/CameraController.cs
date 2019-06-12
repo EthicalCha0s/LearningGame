62179c384154b939e8ae0fd2e5ad9bccabfdb0e6
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-06-12 19:24:36 +0100

    Camera Controller update
    Camera now hides edges of game area more reliably based on rectangle of the tilemap (more reliable)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,6 +9,10 @@ public class CameraController : MonoBehaviour {
     public Tilemap theMap;
     public Vector3 bottomLeft; //Bottom left camera limit
     public Vector3 topRight; //top right camera limit
+
+    public float halfHeight;
+    public float halfWidth;
+
     public float smoothSpeed = 0.05f;
     public int maxX, maxY, minX, minY;
 
@@ -19,19 +23,23 @@ public class CameraController : MonoBehaviour {
         if (minY > maxY || minX > maxX) {
             throw (new UnityException("CameraController.cs script badly initialized"));
         }
+        //getting the camera size
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
 
-        bottomLeft = theMap.localBounds.min;
-        topRight = theMap.localBounds.max;
-
+        //setting the camera bounds
+        bottomLeft = theMap.localBounds.min+ new Vector3(halfWidth,halfHeight,0f);
+        topRight = theMap.localBounds.max + new Vector3(halfWidth,halfHeight,0f);
+        
         PlayerController.instance.camera = this;
     }
 
     // LateUpdate is called once per frame after all Updates    
     void LateUpdate() {
-        Vector3 desiredPosition = 
+        Vector3 desiredPosition =
             new Vector3(//Keeps camera within set bounds
                 Mathf.Clamp(target.position.x, bottomLeft.x, topRight.x),
-                Mathf.Clamp(target.position.y, bottomLeft.y, topRight.y), 
+                Mathf.Clamp(target.position.y, bottomLeft.y, topRight.y),
                 this.transform.position.z);
 
         //smoothes camera opsition
