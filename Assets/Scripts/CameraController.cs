fa28d8eb63ab51bdc63a0ea77dac3576190e7093
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-06-12 19:16:40 +0100

    Updated camera controller
    Camera bounds are now set on the ground tilemap of the scene

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour {
 
     public Transform target;
+    public Tilemap theMap;
+    public Vector3 bottomLeft; //Bottom left camera limit
+    public Vector3 topRight; //top right camera limit
     public float smoothSpeed = 0.05f;
     public int maxX, maxY, minX, minY;
 
@@ -16,28 +20,21 @@ public class CameraController : MonoBehaviour {
             throw (new UnityException("CameraController.cs script badly initialized"));
         }
 
+        bottomLeft = theMap.localBounds.min;
+        topRight = theMap.localBounds.max;
+
         PlayerController.instance.camera = this;
     }
 
     // LateUpdate is called once per frame after all Updates    
     void LateUpdate() {
-        Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, this.transform.position.z);
+        Vector3 desiredPosition = 
+            new Vector3(//Keeps camera within set bounds
+                Mathf.Clamp(target.position.x, bottomLeft.x, topRight.x),
+                Mathf.Clamp(target.position.y, bottomLeft.y, topRight.y), 
+                this.transform.position.z);
 
-        // statements below make it impossible for the camera to go out of bounds
-        if (desiredPosition.x >= maxX) {
-            desiredPosition.x = maxX;
-        }
-        else if (desiredPosition.x <= minX) {
-            desiredPosition.x = minX;
-        }
-
-        if (desiredPosition.y >= maxY) {
-            desiredPosition.y = maxY;
-        }
-        else if (desiredPosition.y <= minY) {
-            desiredPosition.y = minY;
-        }
-
+        //smoothes camera opsition
         Vector3 smoothedPosition = Vector3.Lerp(this.transform.position, desiredPosition, smoothSpeed); //Camera smoothes towards desired position
         this.transform.position = smoothedPosition;
     }
