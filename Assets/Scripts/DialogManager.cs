31deba5096310fa0dc1d41607bed7a0e0fbc14a9
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-06-13 22:25:52 +0100

    Started work on dialogue in test scene (A little messy for now)
    This commit is a little sketch for the Test.unity page, but you can see that whenever you click or press left ctrl the character's text updates.

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text dialogText;
+    public Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    public string[] dialogLines;
+    public int currentline;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        dialogText.text = dialogLines[currentline];
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (dialogBox.activeInHierarchy) {
+            if (Input.GetButtonUp("Fire1")) {
+                currentline++;
+                if (currentline == dialogLines.Length) {
+                    dialogBox.SetActive(false);
+                    currentline = 0;
+                }
+                else {
+                    dialogText.text = dialogLines[currentline];
+                }
+            }
+        }
+
+
+
+    }
+}
