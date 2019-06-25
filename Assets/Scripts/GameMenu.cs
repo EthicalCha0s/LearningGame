8f22b16b45996616f2f5b2ac04abf4b5e145392c
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-06-25 19:05:05 +0100

    Toggle menu by pressing ALT or Right click
    Menu can now be toggled by pressing ALT or right clicking

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+
+    public GameObject theMenu;
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire2")) {
+            if (theMenu.activeInHierarchy) {
+                theMenu.SetActive(false);
+            }
+            else {
+                theMenu.SetActive(true);
+            }
+        }
+    }
+}
