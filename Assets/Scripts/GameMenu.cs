cc0ba35f21738d600c012250bd09b065fb16e3e6
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2019-06-27 02:32:12 +0100

    menu optimisation for singleplayer
    The menu now works perfectly for singleplayer use with a fuller singleplayer menu (thinking of showing equipped items in the main tab later)

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -14,6 +14,13 @@ public class GameMenu : MonoBehaviour
     public Image[] characterImage;
     public GameObject[] charStatHolder;
 
+    public Text nameTextSingle, hpTextSingle, expTextSingle, mpTextSingle, lvlTextSingle;
+    public Slider expSliderSingle, hpSliderSingle, mpSliderSingle;
+    public Image characterImageSingle;
+    public GameObject charStatHolderSingle;
+
+    private int activePlayerCount,activePlayerPos;
+
     // Start is called before the first frame update
     void Start() {
 
@@ -37,34 +44,65 @@ public class GameMenu : MonoBehaviour
         }
 
         //test 
-        /**
         if (theMenu.activeInHierarchy) {
             UpdateMainStats();
-        }**/
+        }
     }
 
     public void UpdateMainStats() {
         playerStats = GameManager.instance.playerStats;
 
+        activePlayerCount = 0;
+
         for (int i = 0; i < playerStats.Length; i++) {
             if (playerStats[i].gameObject.activeInHierarchy) {
-                //Show the active player in the menu
-                charStatHolder[i].SetActive(true);
-
-                //Update their stats in the menu
-                nameText[i].text = playerStats[i].charName;
-                hpText[i].text = "HP: " + playerStats[i].currentHP + "/" + playerStats[i].maxHP;
-                mpText[i].text = "MP: " + playerStats[i].currentMP + "/" + playerStats[i].maxMP;
-                lvlText[i].text = "Lvl: " + playerStats[i].playerLevel;
-                expText[i].text = "" + playerStats[i].currentExp + "/" + playerStats[i].expToNextLevel[playerStats[i].playerLevel];
-                expSlider[i].maxValue = playerStats[i].expToNextLevel[playerStats[i].playerLevel];
-                expSlider[i].value = playerStats[i].currentExp;
-                characterImage[i].sprite = playerStats[i].charImage;
-            }
-            else {
-                charStatHolder[i].SetActive(false);
+                activePlayerCount++;
+                activePlayerPos = i;
             }
         }
-    }
 
+        if (activePlayerCount > 1) {
+            charStatHolderSingle.SetActive(false);
+            for (int i = 0; i < playerStats.Length; i++) {
+                if (playerStats[i].gameObject.activeInHierarchy) {
+                    //Show the active player in the menu
+                    charStatHolder[i].SetActive(true);
+
+                    //Update their stats in the menu
+                    nameText[i].text = playerStats[i].charName;
+                    hpText[i].text = "HP: " + playerStats[i].currentHP + "/" + playerStats[i].maxHP;
+                    mpText[i].text = "MP: " + playerStats[i].currentMP + "/" + playerStats[i].maxMP;
+                    lvlText[i].text = "Lvl: " + playerStats[i].playerLevel;
+                    expText[i].text = "" + playerStats[i].currentExp + "/" + playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                    expSlider[i].maxValue = playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                    expSlider[i].value = playerStats[i].currentExp;
+                    characterImage[i].sprite = playerStats[i].charImage;
+                }
+                else {
+                    charStatHolder[i].SetActive(false);
+                }
+            }
+        }
+        else if (activePlayerCount == 1) {
+            charStatHolder[0].SetActive(false);
+            charStatHolderSingle.SetActive(true);
+
+            nameTextSingle.text = playerStats[activePlayerPos].charName;
+            hpTextSingle.text = "" + playerStats[activePlayerPos].currentHP + "/" + playerStats[activePlayerPos].maxHP;
+            mpTextSingle.text = "" + playerStats[activePlayerPos].currentMP + "/" + playerStats[activePlayerPos].maxMP;
+            lvlTextSingle.text = "Lvl: " + playerStats[activePlayerPos].playerLevel;
+            expTextSingle.text = "" + playerStats[activePlayerPos].currentExp + "/" + playerStats[activePlayerPos].expToNextLevel[playerStats[activePlayerPos].playerLevel];
+
+            expSliderSingle.maxValue = playerStats[activePlayerPos].expToNextLevel[playerStats[activePlayerPos].playerLevel];
+            expSliderSingle.value = playerStats[activePlayerPos].currentExp;
+
+            hpSliderSingle.maxValue = playerStats[activePlayerPos].maxHP;
+            hpSliderSingle.value = playerStats[activePlayerPos].currentHP;
+
+            mpSliderSingle.maxValue = playerStats[activePlayerPos].maxMP;
+            mpSliderSingle.value = playerStats[activePlayerPos].currentMP;
+
+            characterImageSingle.sprite = playerStats[activePlayerPos].charImage;
+        }
+    }
 }
